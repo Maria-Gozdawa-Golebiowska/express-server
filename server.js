0613dbc43f41a6ecbf29154051ce7cad6adb1501
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-08-26 15:41:31 +0200

    second task

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars")
 
 const app = express();
+app.engine("hbs", hbs());
+app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
 app.get("/", (req, res) => {
-  res.show("home.html");
+  res.render("index");
 });
 
-app.get("/home", (req, res) => {
-  res.show("home.html");
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about", { layout: 'dark' }); 
 });
+
 
 app.use("/user", (req, res) => {
-  res.show("forbidden.html");
+  res.render("forbidden");
 });
 
-// Obsługa błędu 404
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/info", (req, res) => {
+  res.render("info");
+});
+
+app.get("/history", (req, res) => {
+  res.render("history");
+});
+
 app.use((req, res) => {
-  res.status(404).show("404.html");
+  res.status(404).send("404 page not found...");
 });
 
 app.listen(8000, () => {
